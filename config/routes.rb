e249cf1d0973004915d39e98f8cc5f3e46aed4ee
hadeelfouad <hadeel.fouad15@gmail.com>
2015-03-02 19:29:38 +0200

    view all ideas related to forum

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,11 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :forums
+  resources :forums do
+    resources :ideas do
+      resources :comments
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
