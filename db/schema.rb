3cbaf63837728ff0e884fef6fd6ec342a1227f7e
Ahmed Osama <ahmed.osama94@gmail.com>
2015-03-12 15:16:59 +0100

    Added a new column to the Notifications table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312034633) do
+ActiveRecord::Schema.define(version: 20150312140624) do
 
   create_table "forums", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150312034633) do
     t.boolean  "seen"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
