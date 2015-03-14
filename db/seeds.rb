c79348bcbeabc1c07d4a54a2b704c084bfd1e77e
Ahmed Osama <ahmed.osama94@gmail.com>
2015-03-14 02:34:13 +0100

    Added a warning in seeds.rb and added a test for notifications routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
-User.create(email: 'dovahkiin@gmail.com', password: '12345678', password_confirmation: '12345678', username: 'Dovahkiin', gender: 'male', full_name: 'Dovahkiin Dovah Dov', password_question: 'what?', answer_for_password_question: 'yes')
-#Notification.create(info: 'NEW NOTIFICATION BITCH', seen: false, user_id: 1)
-#Forum.create(title: 'araf', description:'araf aktar', privacy: '1')
-#Idea.create(text: 'araf begad', forum_id: 1, user_id: 1)
-#Notification.find(2).destroy
-#Notification.find(3).destroy
-#Notification.find(4).destroy
-#Forum.find(2).destroy
-#Forum.find(3).destroy
+# IF YOU DO NOT REMOVE THE SEED CREATIONS/DELETIONS/... THEY WILL BE EXECUTED EVERY TIME YOU INVOKE RAKE DB:SEED
--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class NotificationsControllerTest < ActionController::TestCase
+	test "user/:user_id/notifications is working" do
+		get :index
+		assert_response :success
+	end
 end
