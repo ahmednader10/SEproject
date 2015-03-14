5d2e68786b12a154f7cf69ec35502cf306664afa
Nader <ahmednader839@gmail.com>
2015-03-14 23:00:30 +0100

    joining forum

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150313113335) do
     t.datetime "updated_at"
   end
 
+  create_table "ideas", force: true do |t|
+    t.string   "text"
+    t.integer  "forum_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "memberships", force: true do |t|
     t.integer  "forum_id"
     t.integer  "user_id"
