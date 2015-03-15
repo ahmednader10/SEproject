48e4d3048d23b5b8d574d406c84a068bc93b7e14
Ahmed Osama <ahmed.osama94@gmail.com>
2015-03-15 01:56:57 +0100

    Modified the notifications page to open according to the current session

--- config/routes.rb
+++ config/routes.rb
@@ -28,9 +28,7 @@ Rails.application.routes.draw do
 
   post 'forums/:id/join' => 'forums#join_forum', as:'join_forum'
 
-  get 'users/:user_id/notifications' => 'notifications#index', as: 'user_notifications'
-
-  get 'users/:user_id/notifications/:id' => 'notifications#show', as: 'user_notification'
+  get '/notifications' => 'notifications#index', as: 'user_notifications'
 
   get 'users/indentation_error_message' => 'users#indentation_error_message'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313113335) do
+ActiveRecord::Schema.define(version: 20150312140625) do
 
   create_table "forums", force: true do |t|
     t.string   "title"
