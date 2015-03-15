110af5f72de45352a72ba7cf9e81596ee79181c9
miadanour <miadanour@gmail.com>
2015-03-15 20:52:56 +0200

    ma3'ayartesh 7aaga ma3rafsh

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,8 @@ class FriendshipsController < ApplicationController
 def index
 	@users=User.all 
 end 
-	def create
+	
+def create
   @friendship = Friendship.new(:user_id => current_user.id , :friend_id => params[:friend_id] , :user_name => current_user.username , :friend_name => User.find_by(id: params[:friend_id]).username)
   if @friendship.save
     flash[:notice] = "Added friend."
@@ -13,12 +14,11 @@ end
     redirect_to root_url
   end
 end
+
 def show 
 	@users=User.all 
 	@friendships=Friendship.all
-	end 
-
-
+end 
 
 def destroy
   @friendship = current_user.friendships.find(params[:id])
