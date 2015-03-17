99f7e52aa8c96ea26b428f2d29b9d200bd5b20cd
hadeelfouad <hadeel.fouad15@gmail.com>
2015-03-17 12:21:10 +0100

    changes in comment_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,36 @@
 class CommentsController < ApplicationController
+	before_action :authenticate_user, only: :create
+
 	#index method list all the comments related to an idea.
 	def index
-		@comments = Comment.where(ideaid: params[:idea_id])
+		@comments = Comment.where(idea_id: params[:idea_id])
 	end
 
-   
+   def new
+   	@idea = idea.find(params[:idea_id])
+		@comment = Comment.new
+   end
+
    #enables the user to post a comment on an idea.
 	def create
-		@forum = Forum.find(params[:forum_id])
-		@idea = Idea.find(params[:idea_id])
-		@user = current_user
-		if current_user == nil
+		@comment = @idea.comment.build(comment_params)
+		@comment.user = current_user
+
+		if 
+			@comment.save
+			redirect_to @idea
 
 		else
-			@comment = Comment.new(comment_params)
-			@comment.save
-			redirect_to "ideas/show"
+
+			render action: :new
+
 		end
 	end
 
 # used to allow the user to enter the information needed from him and nothing more inorder not to be able to change the model
 protected
 	def comment_params
-		params.require(:comment).permit(:userid, :forumid, :ideaid, :text)
+		params.require(:comment).permit(:text)
 	end
 end
 
