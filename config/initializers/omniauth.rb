3b10cf6cbc531b360c2c870c615b0255f51039d3
miadanour <miadanour@gmail.com>
2015-03-17 04:52:57 +0200

    fixes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,11 @@
 OmniAuth.config.logger = Rails.logger
 
+#Rails.application.config.middleware.use OmniAuth::Builder do
+ # provider :facebook, "1392586531057897", "298b16bf0dc5597eb2c143dc8578f7bf"
+# end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "1392586531057897", "298b16bf0dc5597eb2c143dc8578f7bf"
+    provider :facebook, "1392586531057897", "298b16bf0dc5597eb2c143dc8578f7bf",  {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}, :scope => 'publish_stream,email'}
 end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, "2piBf8UAgv4xtZxgpNE8paxSj", "t5w86U4A4uFStC7tFZ7LSUcWfpyI9AUXOrHO6eYwZNV3BvUB65"
 end
