d36f4b3a27d1ae6fe24023e63ead042aef7891c6
Omar <o.abouzaid93@gmail.com>
2015-04-12 19:21:54 +0200

    all unit tests for sign up validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,39 @@ require 'test_helper'
 
 
 class UserTest < ActiveSupport::TestCase
-  def test_valid_user
-  	user = users(:user_with_valid_data)
-  	assert user.valid?, user.errors.full_messages.inspect
+  #def test_valid_user
+  #	user = users(:user_with_valid_data)
+  #	assert user.valid?, user.errors.full_messages.inspect
+  #end
+
+  def test_empty_email
+  	user = users(:user_without_an_email)
+  	assert !user.valid?  
   end
+
+  def test_short_password
+  	user = users(:user_with_short_password)
+  	assert !user.valid?
+  end
+
+  def test_empty_password
+  	user = users(:user_with_no_password) 
+  	assert !user.valid?
+  end
+
+  def test_empty_username
+  	user = users(:user_with_no_username)
+  	assert !user.valid?
+  end
+
+  def test_repeated_email
+  	user = users(:user_with_repeated_email)
+  	assert !user.valid?
+  end
+
+  def test_repeated_username
+  	user = users(:user_with_repeated_username)
+  	assert !user.valid?
+  end
+
 end
