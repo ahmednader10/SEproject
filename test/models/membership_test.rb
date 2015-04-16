1480a7b2783fe42e66ceb83bd7cd39b1cd2ed8f7
Nader <ahmednader839@gmail.com>
2015-04-16 02:27:52 +0200

    no changes

--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
+
 class MembershipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  #def test_valid_user
+  #	user = users(:user_with_valid_data)
+  #	assert user.valid?, user.errors.full_messages.inspect
+  #end
+
+  def test_duplicate_memberships
+  	membership1 = memberships(:one)
+  	membership2 = memberships(:two)
+  	assert !membership2.valid?,"can't join same forum multiple times"
+  end
+
+  def test_different_memberships
+  	membership1 = memberships(:one)
+  	membership2 = memberships(:three)
+  	assert membership2.valid?,"can't join different forums"
+  end
 end
