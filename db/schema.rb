285babdfc8d647442dade2b735248d60a6adea27
miadanour <miadanour@gmail.com>
2015-04-16 07:26:51 +0200

    user privacy added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412184615) do
+ActiveRecord::Schema.define(version: 20150416044159) do
 
   create_table "admins", force: true do |t|
     t.integer  "forum_id"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20150412184615) do
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.integer  "privacy"
   end
 
 end
