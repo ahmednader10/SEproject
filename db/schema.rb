104a35380b66cac3fbcc76adf054721fc3381ad2
miadanour <miadanour@gmail.com>
2015-04-16 15:20:56 +0200

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416044159) do
+ActiveRecord::Schema.define(version: 20150416131913) do
 
   create_table "admins", force: true do |t|
     t.integer  "forum_id"
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 20150416044159) do
     t.string   "name"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.integer  "privacy"
+    t.integer  "privacy",                      default: 1
   end
 
 end
