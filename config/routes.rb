d93f71f7afb702a3a9fbc857ef94612c830fbf2f
Ahmed Osama <ahmed.osama94@gmail.com>
2015-04-19 02:47:36 +0200

    Added partitioning to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -76,7 +76,7 @@ Rails.application.routes.draw do
   #Change later
 
   get     'logged_in' => 'sessions#logged_in'
-  ###########################################################
+  ############################ Users ###############################
 
   get 'users/index'
 
@@ -94,6 +94,8 @@ Rails.application.routes.draw do
   get '/users/:id' => 'users#show'
 
   get '/users/profile/:id' => 'users#profile'
+
+  ############################ Forums ########################################
 
   get 'forums/created/:id' => 'forums#created', as: 'created'
 
