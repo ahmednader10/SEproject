5fe9638f5d54c8a6855d14732d39e3efc8550b8f
mohaazeem <mohaazeem7@gmail.com>
2015-04-20 01:31:45 +0200

    testing

--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -4,4 +4,9 @@ class ForumTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save forum without title" do
+  	forum = Forum.new
+  	assert_not forum.save
+  end
 end
