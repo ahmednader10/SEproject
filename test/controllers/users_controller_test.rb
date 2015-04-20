7977011aaa4b8c96fd19b24ab18f37cfef3ea75d
Nader <ahmednader839@gmail.com>
2015-04-20 02:20:28 +0200

    some functional tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -31,17 +31,23 @@ class UsersControllerTest < ActionController::TestCase
     #assert_response :success
   end
 
-  test "should get admin_join_forums_requests" do
-      get :admin_join_forums_requests
-      assert_response :success
-
+  test "should route to admin_join_forums_requests" do
+      assert_generates 'users/join_requests', {controller: 'users' , action: 'admin_join_forums_requests'}
+    end
 
   test "should show user" do
-  assert_generates '/users/1', { controller: 'users', action: 'show', id: '1' }
-
+    assert_generates '/users/1', { controller: 'users', action: 'show', id: '1' }
   end
 
   test "should get profile"  do
     assert_generates 'users/profile/1', {controller: 'users' , action: 'profile', id:'1'}
+  end
+
+  test "should route to accept join request"  do
+    assert_generates 'users/accept_join_request', {controller: 'users' , action: 'accept_join_request'}
+  end
+
+  test "shouldroute to reject join request"  do
+    assert_generates 'users/reject_join_request', {controller: 'users' , action: 'reject_join_request'}
   end
 end
