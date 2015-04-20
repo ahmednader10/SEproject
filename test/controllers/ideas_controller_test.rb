843445cd0836a8803fb111500d614efe16d2b3d8
Nader <ahmednader839@gmail.com>
2015-04-20 22:22:54 +0200

    -

--- test/controllers/ideas_controller_test.rb
+++ test/controllers/ideas_controller_test.rb
@@ -2,6 +2,9 @@ require 'test_helper'
 
 class IdeasControllerTest < ActionController::TestCase
 	test "should route to remove idea" do
-  	assert_generates 'forums/1/ideas/4', {controller: 'ideas' , action: 'destroy', id:'4', forum_id:'1'}
-  end
+  		assert_generates 'forums/1/ideas/4', {controller: 'ideas' , action: 'destroy', id:'4', forum_id:'1'}
+  	end
+
+  	
+
 end
