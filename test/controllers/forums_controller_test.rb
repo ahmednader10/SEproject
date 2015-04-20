4e7ddb80ca2f73ca1f9b8e34dffe8613552d90f9
mohaazeem <mohaazeem7@gmail.com>
2015-04-21 00:12:24 +0200

    pulling

--- test/controllers/forums_controller_test.rb
+++ test/controllers/forums_controller_test.rb
@@ -4,4 +4,50 @@ class ForumsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+  	get :index
+  	assert_response :success
+  	assert_not_nil assigns(:forums)
+  end
+
+  test "should get new" do
+  	get :new
+  	assert_response :success
+  	assert_not_nil assigns(:forum)
+  end
+
+  test "should get edit" do
+  	get(:edit, {'id' => "7"})
+  	assert_response :success
+  	assert_not_nil assigns(:forum)
+  end
+
+  test "should get created" do
+  	get(:created, {'id' => "10"})
+  	assert_response :success
+  	assert_not_nil assigns(:forum)
+  end
+
+  test "should get show" do
+  	get(:show, {'id' => "3"}, {'user_id' => "1"})
+  	assert_response :success
+  	assert_not_nil assigns(:forum)
+  end
+
+  test "should create forum" do
+  	assert_difference('Forum.count') do
+  		assert_difference('Admin.count') do
+  			assert_difference('Membership.count') do
+  				session[:user_id] = users(:user_with_valid_data).id
+  				post(:create, forum: {title: 'title', description: 'description', privacy: '1'})
+  			end
+  		end
+  	end
+  	assert_redirected_to created_path(assigns(:forum))
+  end
+
+  test "should update forum" do
+  	
+  end
 end
--- test/models/forum_test.rb
+++ test/models/forum_test.rb
@@ -6,7 +6,24 @@ class ForumTest < ActiveSupport::TestCase
   # end
 
   test "should not save forum without title" do
-  	forum = Forum.new
+  	forum = forums(:forum_no_title)
   	assert_not forum.save
   end
+
+  test "should save forum with title" do
+  	forum = forums(:forum_title)
+  	assert forum.save
+  end
+
+  test "should not save forum without privacy setting" do
+  	forum = forums(:forum_no_privacy)
+  	assert_not forum.save
+  end
+
+  test "should save forum with privacy setting" do
+  	forum = forums(:forum_privacy)
+  	assert forum.save
+  end
+
+  
 end
