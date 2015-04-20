43710f89fb5b8719849637ecaffc76e640d337ac
Omar <o.abouzaid93@gmail.com>
2015-04-20 22:17:55 +0200

    test friendship_controller_test.rb should be re-done.

--- test/controllers/sysadmins_controller_test.rb
+++ test/controllers/sysadmins_controller_test.rb
@@ -11,15 +11,26 @@ class SysadminsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  #test "should get show" do
-  #  get :show
+  test "should get show" do
+    session[:sysadmin] = "true"
+    post :show, session[:sysadmin]
+    assert_response :success
+  end
+
+  #test "should not get show" do
+  #  current_user = users(:user_with_valid_data)
+  #  session[:sysadmin] = "true"
+  #  post :show, session[:sysadmin], current_user
   #  assert_response :success
   #end
 
-  #test "should get edit" do
-  #  get :edit
-  #  assert_response :success
-  #end
+  test "should get edit" do
+    #session[:email] = "omar.ashraf@gmail.com"
+    get :edit
+    assert_response :success
+    assert_not_nil assigns(:users)
+    #assert_not_nil assigns(:user_tmp)
+  end
 
   test "should get delete" do
     get :delete
