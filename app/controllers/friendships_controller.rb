d29a274bee9ee72c2393bc4c2512e476043c0e4b
raghda94 <raghdasoltan1994@gmail.com>
2015-04-20 22:36:36 +0200

    removed some tests

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user, only: [:create]
 
 
 def show
- 
+ @users= User.all
   @friend = User.find(params[:id])
 
 end 
@@ -21,14 +21,14 @@ def create
 @user = current_user
 @friend = User.find( params[:friend_id])
 
-@friendship1 = Friendship.new(:user_id => @user.id , :friend_id => @friend.id,  :user_name => @user.username, :friend_name =>@friend.username, :status => 0, :requester => @user.username , :requested => @friend.username)
-@friendship2 = Friendship.new(:user_id => @friend.id ,:friend_id => @friend.id, :user_name => @friend.username , :requests => @user.username)
+@friendship = Friendship.new(:user_id => @user.id , :friend_id => @friend.id,  :user_name => @user.username, :friend_name =>@friend.username, :status => 0, :requester => @user.username , :requested => @friend.username)
+
 
 Action.create(info: @user.username + ' has sent a friend request to ' + @friend.username, user_id: @user.id)
 
 
 
-  if @friendship1.save  && @friendship2.save
+  if @friendship.save  
     flash[:success] = "Added friend."
     redirect_to root_path
   else
@@ -41,11 +41,11 @@ def update
 @user = User.find(current_user)
 @friend = Friendship.find( params[:id])
 
-@friendship1 = Friendship.find_by( user_id: @user.id,friend_id: @friend.friend_id)
-@friendship2 = Friendship.find_by( user_id: @friend.friend_id,friend_id: @user.id)
+@friendship = Friendship.find_by( user_id: @friend.user_id,friend_id: @user.id)
+#@friendship2 = Friendship.find_by( user_id: @friend.friend_id,friend_id: @user.id)
 
 
-if @friendship1.update_attributes(:user_id => @user.id, :friend_id => @friend.user_id, :status => 1) && @friendship1.update_attributes(:friend_id => @user.id, :user_id => @friend.user_id, :status => 1)
+if @friendship.update_attributes(user_id: @friend.user_id,friend_id: @user.id, status: 1) 
 flash[:notice] = 'Friend sucessfully accepted!'
 redirect_to friendships_path
 else
--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -37,27 +37,28 @@ class FriendshipsControllerTest < ActionController::TestCase
 end 
 	
 
-test "should get delete" do
-    get :destroy
-    assert_response :success
+test "should show friendships" do
+    assert_generates '/friendships/1', {controller: 'friendships', action: 'show', id: '1'}
   end
 
-test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
+#test "should get index" do
+    #get :index
+    #assert_response :success
+    #assert_not_nil assigns(:users)
+  #end
 
 
 
-test "should create a friendship"  do 
-	assert_difference('Friendship.count') do
-		post :create , friendship1: {user_id: 2 , friend_id: 3 ,  user_name: rowan, friend_name: raghda, :status => 0, requester: => rowan , requested: raghda} ,friendship2:{user_id: 3 ,friend_id:2 , user_name: raghda , requests: => rowan}
-end 
-assert_response :success
-#assert_redirected_to users_path(assigns(:user))
 
-end
+
+
+#test "should route to accept friend request" do
+
+	#assert_generates 'friendships/update' , {controller: 'friendships' , action: 'update'}
+  
+#end 
+
+
 
 
 
