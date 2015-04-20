7d6a5e8b34fa6f54f7ccd54218a15275ef8da388
Ahmed Osama <ahmed.osama94@gmail.com>
2015-04-20 02:27:14 +0200

    Created functional tests for notifications

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class NotificationsControllerTest < ActionController::TestCase
-	test "should get index" do
+	
+	test "should get notifications" do
 		get :index
-		assert_response :success
+		assert_response :success, "Failed to get notifications"
+	end
+
+	test "should redirect to notifications" do
+		delete :destroy
+		assert_redirected_to (controller: "notifications", action: "index")
 	end
 	
 end
