59889c3fcdd6f3050c70a0bdac05b6e21a602f32
Omar <o.abouzaid93@gmail.com>
2015-04-21 07:15:08 +0200

    Documentation and fixing tests again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -164,6 +164,7 @@ class UsersController < ApplicationController
       def profile
         @current_user = current_user
       end
+      
     # user_params action requires the model user and whenever we want to retrieve the user's parameteres
     # we can do so using this action. Also it prevents a user from hacking into the app and changing the
     # model.
