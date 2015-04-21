60b5d5e582af7d8e5d6292a9e731dc407079a46d
miadanour <miadanour@gmail.com>
2015-04-21 06:37:28 +0200

    fix

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 
+
+
   # This test checks that a request to "index" action was successful and also makes sure
   # that the variable "users" has some data since the controller do so to this variable.
   test "should get index" do
@@ -56,14 +58,6 @@ class UsersControllerTest < ActionController::TestCase
       assert_generates 'users/join_requests', {controller: 'users' , action: 'admin_join_forums_requests'}
     end
 
-  test "should show user" do
-    assert_generates '/users/1', {controller: 'users', action: 'show', id: '1'}
-  end
-
-  test "should get profile"  do
-    assert_generates 'users/profile/1', {controller: 'users' , action: 'profile', id:'1'}
-  end
-
 
   test "should route to accept join request"  do
     assert_generates 'users/accept_join_request', {controller: 'users' , action: 'accept_join_request'}
@@ -72,5 +66,36 @@ class UsersControllerTest < ActionController::TestCase
   test "shouldroute to reject join request"  do
     assert_generates 'users/reject_join_request', {controller: 'users' , action: 'reject_join_request'}
   end
+
+#this test show that a user can edit his information and that after the information is updated the page redirects back to itself to patch the informatiom
+
+test "should edit user" do
+  @user = users(:miada)
+  patch :update , id: @user.id
+  assert_response :redirect 
+end
+
+#tests the generated url from showing a user
+test "should generate user route" do
+    assert_generates '/users/1', {controller: 'users', action: 'show', id: '1'}
+  end
+
+#tests the show action 
+test "should show user"do
+@user =users(:miada)
+assert_response :success
+get :show , id: @user.id
+end  
+
+test "should redirect to profile" do
+
+end
+
+#tests the generated url from showing a user's profile
+  test "should get profile"  do
+    assert_generates 'users/profile/1', {controller: 'users' , action: 'profile', id:'1'}
+  end
+
+
 
 end
