980279793b73abf66a1183def1324dfc2e598276
raghda94 <raghdasoltan1994@gmail.com>
2015-05-05 19:58:11 +0200

    Fixes

--- db/migrate/20150503125129_add_count_to_blockers.rb
+++
@@ -1,7 +0,0 @@
-class AddCountToBlockers < ActiveRecord::Migration
-  def change
-  	add_column :blockers , :blocked_count , :int , column_options: {null: false} , default: 0 
-  	Blocker.each {|b| Blocker.reset_counters b , :blocked } 
-  	
-  end
-end
