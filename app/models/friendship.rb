3c024dfc14fc078e1f97f1922ab3dc093b1bab43
raghda94 <raghdasoltan1994@gmail.com>
2015-05-07 01:26:05 +0200

    Fixes

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -7,7 +7,9 @@ class Friendship < ActiveRecord::Base
 	validates :user_id , :presence => true
 	validates :friend_id , :presence => true
 
-
+ 
+ has_many :bfriends , :through => :blockers , source: :user
+    has_many :rfriends , :through => :report_users , source: :user
 
 	
 	
