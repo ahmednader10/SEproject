7e25f2d8a7535487f33fb49c77f6c99859f97707
Ahmed Osama <ahmed.osama94@gmail.com>
2015-05-08 19:39:23 +0200

    the search now has a restriction list for the search queries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,18 @@ class SearchController < ApplicationController
 	
 	def search
 		if current_user != nil && params[:q] != nil
-			Action.create(info: current_user.username + ' has made the following search query : (' + params[:q] + ').', user_email: current_user.email)		
-			@results = User.where("UPPER(username) = UPPER(:q) or UPPER(full_name) = UPPER(:q)", q: params[:q])
-			@results += Forum.where("UPPER(title) = UPPER(:q)", q: params[:q])
-			@results += Idea.where("UPPER(title) = UPPER(:q)", q: params[:q])
+			Action.create(info: current_user.username + ' has made the following search query : (' + params[:q] + ') for (type:' + params[:type] + ').', user_email: current_user.email)		
+			if params[:type] == 'All'
+				@results = User.where("UPPER(username) = UPPER(:q) or UPPER(full_name) = UPPER(:q)", q: params[:q])
+				@results += Forum.where("UPPER(title) = UPPER(:q)", q: params[:q])
+				@results += Idea.where("UPPER(title) = UPPER(:q)", q: params[:q])
+			elsif params[:type] == 'Users'
+				results = User.where("UPPER(username) = UPPER(:q) or UPPER(full_name) = UPPER(:q)", q: params[:q])
+			elsif params[:type] == 'Forums'
+				@results = Forum.where("UPPER(title) = UPPER(:q)", q: params[:q])
+			elsif params[:type] == 'Ideas'
+				@results = Idea.where("UPPER(title) = UPPER(:q)", q: params[:q])
+			end
 		end
 	end
 
