92d48409bb70f1ab9d3e0752b43407945de937c0
Ahmed Osama <ahmed.osama94@gmail.com>
2015-05-08 19:14:58 +0200

    search now is case-insensitive

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
 	def search
 		if current_user != nil && params[:q] != nil
 			Action.create(info: current_user.username + ' has made the following search query : (' + params[:q] + ').', user_email: current_user.email)		
-			@results = User.where(username: params[:q])
-			@results += Forum.where(title: params[:q])
-			@results += Idea.where(title: params[:q])
+			@results = User.where("UPPER(username) = UPPER(:q) or UPPER(full_name) = UPPER(:q)", q: params[:q])
+			@results += Forum.where("UPPER(title) = UPPER(:q)", q: params[:q])
+			@results += Idea.where("UPPER(title) = UPPER(:q)", q: params[:q])
 		end
 	end
 
