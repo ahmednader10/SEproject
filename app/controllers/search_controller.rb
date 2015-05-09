a23daac3caca866b93d4ce655280de99789c9f28
Ahmed Osama <ahmed.osama94@gmail.com>
2015-05-09 21:15:24 +0200

    You can now make queries with partial strings

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,15 +4,15 @@ class SearchController < ApplicationController
 		if current_user != nil && params[:q] != nil
 			Action.create(info: current_user.username + ' has made the following search query : (' + params[:q] + ') for (type:' + params[:type] + ').', user_email: current_user.email)		
 			if params[:type] == 'All'
-				@results = User.where("UPPER(username) = UPPER(:q) or UPPER(full_name) = UPPER(:q)", q: params[:q])
-				@results += Forum.where("UPPER(title) = UPPER(:q)", q: params[:q])
-				@results += Idea.where("UPPER(title) = UPPER(:q)", q: params[:q])
+				@results = User.where("UPPER(username) LIKE ('%' || UPPER(:q) || '%') OR UPPER(full_name) LIKE ('%' || UPPER(:q) || '%')", q: params[:q])
+				@results += Forum.where("UPPER(title) LIKE ('%' || UPPER(:q) || '%')", q: params[:q])
+				@results += Idea.where("UPPER(title) LIKE ('%' || UPPER(:q) || '%')", q: params[:q])
 			elsif params[:type] == 'Users'
-				results = User.where("UPPER(username) = UPPER(:q) or UPPER(full_name) = UPPER(:q)", q: params[:q])
+				results = User.where("UPPER(username) LIKE ('%' || UPPER(:q) || '%') OR UPPER(full_name) LIKE ('%' || UPPER(:q) || '%')", q: params[:q])
 			elsif params[:type] == 'Forums'
-				@results = Forum.where("UPPER(title) = UPPER(:q)", q: params[:q])
+				@results = Forum.where("UPPER(title) LIKE ('%' || UPPER(:q) || '%')", q: params[:q])
 			elsif params[:type] == 'Ideas'
-				@results = Idea.where("UPPER(title) = UPPER(:q)", q: params[:q])
+				@results = Idea.where("UPPER(title) LIKE ('%' || UPPER(:q) || '%')", q: params[:q])
 			end
 		end
 	end
