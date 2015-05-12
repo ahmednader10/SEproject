e59b6d82f33df8be52571ff198e2e1be99b051c4
Ahmed Osama <ahmed.osama94@gmail.com>
2015-05-12 05:00:01 +0200

    foreing keyh

--- app/models/reportidea.rb
+++ app/models/reportidea.rb
@@ -1,6 +1,6 @@
 class Reportidea < ActiveRecord::Base
 
-	belongs_to :idea, foreing_key: :idea_id
+	belongs_to :idea, foreign_key: :idea_id
 
 	validates :user_id, uniqueness: { scope: :idea_id }, presence: true
 	validates :idea_id, presence: true
