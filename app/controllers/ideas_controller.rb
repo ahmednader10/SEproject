c607a0b35fbb061010bcb2ebb57f83d9674c4e89
Omar <o.abouzaid93@gmail.com>
2015-05-20 21:11:11 +0200

    pull

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -87,15 +87,11 @@ class IdeasController < ApplicationController
 		if @likeidea.save
 			Action.create(info: @user.username + ' has liked an idea: (' + @idea.title + ') belonging to user: (' + User.find(@idea.user_id).username + ') located in forum: (' + @forum.title + ').', user_email: @user.email)
 			if @user.id != @idea.user_id
-				<<<<<<< HEAD
-				Notification.create(info: @user.username + ' has liked your idea: (' + @idea.title + ') on forum: (' + @forum.title + ').', user_id: @idea.user_id)
-			end
-			flash[:notice] = "Idea Liked!"
-			=======
+				
 			Notification.create(info: @user.username + ' has liked your idea: (' + @idea.title + ') on forum: (' + @forum.title + ').', user_id: @idea.user_id, link: 'forums/' + @forum.id.to_s + 'ideas/' + @idea.id.to_s)
 		end
 		flash[:notice] = "Idea Liked!"
-		>>>>>>> 3f4a2e470ff80b7a46d222dba14720f96cbc1e35
+		
 	end
 
 	redirect_to forum_idea_path(@forum, @idea) # [@forum, @idea]
